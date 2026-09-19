47f4207b31df69f8be44ff48c44ce6de5aa80ae0
justkinou <justkinou@proton.me>
2026-09-20 00:58:25 +0200

    feat: add logger middleware

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use tonic::transport::Server;
-
+use tower_http::trace::TraceLayer;
 use crate::{core::TokenManager, service::{AuthService, auth::auth_server::AuthServer}};
 
 pub mod config;
@@ -9,6 +9,11 @@ mod core;
 mod service;
 
 pub async fn run(conf: config::Config) -> Result<()> {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
+
+
     let addr = "0.0.0.0:8080".parse()?;
 
     let token_manager = TokenManager::build(conf).await
@@ -16,6 +21,7 @@ pub async fn run(conf: config::Config) -> Result<()> {
     let auth_service = AuthService::new(token_manager);
 
     Server::builder()
+        .layer(TraceLayer::new_for_grpc())
         .add_service(AuthServer::new(auth_service))
         .serve(addr)
         .await?;
